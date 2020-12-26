05f5cc9d2b62aeba6c174dd0fdaf4a50ec0c16c2
Saulus Ribeiro <saulusribeiro@gmail.com>
2020-12-26 19:01:16 -0300

    Criação do banco H2 e da tabela Categoria

--- src/main/java/br/com/srconsultoria/cursomc/domain/Categoria.java
+++ src/main/java/br/com/srconsultoria/cursomc/domain/Categoria.java
@@ -2,16 +2,25 @@ package br.com.srconsultoria.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 // A Interface Serializable permite que os objetos da classe serão convertidos
 // para uma sequencia de bytes, e sejam gravados em arquivos ou
 // trafegar em rede
 
+@Entity
 public class Categoria implements Serializable{
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String  nome;
 	
