59e2fb0235e626fbd83cc1fec77a480d493fa4d5
Saulus Ribeiro <saulusribeiro@gmail.com>
2021-03-26 20:59:44 -0300

    Validação customizada email não repetido na base de clientes

--- src/main/java/br/com/srconsultoria/cursomc/repositories/ClienteRepository.java
+++ src/main/java/br/com/srconsultoria/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,16 @@ package br.com.srconsultoria.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.srconsultoria.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	// O Spring Data implementa a busca apenas com o comando abaixo
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/br/com/srconsultoria/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/com/srconsultoria/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,30 +6,44 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.srconsultoria.cursomc.domain.Cliente;
 import br.com.srconsultoria.cursomc.domain.enums.TipoCliente;
 import br.com.srconsultoria.cursomc.dto.ClienteNewDTO;
+import br.com.srconsultoria.cursomc.repositories.ClienteRepository;
 import br.com.srconsultoria.cursomc.resources.exception.FieldMessage;
 import br.com.srconsultoria.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Autowired
+	private ClienteRepository repo;
+
 	@Override
-	public 	void initialize(ClienteInsert ann) {
+	public void initialize(ClienteInsert ann) {
 	}
 
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 // inclua os testes aqui, inserindo erros na lista
-		
-		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod())&&!BR.isValidCPF(objDto.getCpfcnpj())) {
-            list.add(new FieldMessage("cpfCnpj", "CPF Inválido"));
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfcnpj())) {
+			list.add(new FieldMessage("cpfcnpj", "CPF Inválido"));
 		}
-		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod())&&!BR.isValidCNPJ(objDto.getCpfcnpj())) {
-            list.add(new FieldMessage("cpfCnpj", "CNPJ Inválido"));
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfcnpj())) {
+			list.add(new FieldMessage("cpfcnpj", "CNPJ Inválido"));
 		}
-	
+
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+
 		for (FieldMessage e : list) {
-			// Os comandos abaixo transporta as mensagens e campos personalizados para validação através do framework
+			// Os comandos abaixo transporta as mensagens e campos personalizados para
+			// validação através do framework
 			// Não se preocupar em decorar os comandos
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
